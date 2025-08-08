6d63a87bc9c62fa7c6f8b554e0c397be56db1a0d
kinjal-savitara <kinjal@savitarainfotel.com>
2025-08-08 12:22:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,9 +24,9 @@ export default function Home() {
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <div className="w-full max-w-6xl">
         <div className="relative pb-[56.25%] h-0 overflow-hidden rounded shadow-lg">
-          <iframe src="https://teliportme.com/embed/tour/576e9e57" frameBorder="0" scrolling="no" height="360" width="640" allowFullScreen allow="vr;xr-spatial-tracking;gyroscope;accelerometer;fullscreen;magnetometer;"></iframe>
-        </div>
-      </div>
+         <iframe src="https://tours.savitarrealty.in/embed/tour/576e9e57" frameBorder="0" scrolling="no" height="100%" width="100%" allowFullScreen allow="vr;xr-spatial-tracking;gyroscope;accelerometer;fullscreen;magnetometer;"></iframe>
+
+          </div>
     </div>
   )
 }
